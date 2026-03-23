95381ab7a74d90673a2f932e9439e274aa41b783
Zay-0309 <128918457+Zay-0309@users.noreply.github.com>
2026-03-23 20:48:48 +0100

    Add files via upload

---
+++ Django_Project/Django_project/app/webapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+def welcome(request):
+    return render(request, "welcome.html")
+
+
+def product(request):
+    return render(request, "product.html")
+
+def contact(request):
+    return render(request, "contact.html")
